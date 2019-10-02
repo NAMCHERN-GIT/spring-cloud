d23e65cab6aaadd81c354e82e166b4a2a0033c92
chen.nan <namchern@aliyun.com>
2019-10-02 19:25:02 +0800

    代码追加注释

--- msc-zuul-gateway-11000/src/main/java/com/chennan/cloud/MscZuulGateway_11000_App.java
+++ msc-zuul-gateway-11000/src/main/java/com/chennan/cloud/MscZuulGateway_11000_App.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * springboot 入口启动类
+ * {@link @EnableZuulProxy} 网关代理enable
+ *
+ * @author chen.nan
+ */
 @SpringBootApplication
 @EnableZuulProxy
 public class MscZuulGateway_11000_App {
