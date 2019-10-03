757071d1c03d5e553c1ff923769c6ca105b2736c
chen.nan <namchern@aliyun.com>
2019-10-03 21:41:37 +0800

    微服务文件上传下载代码上传

--- msc-provider-hystrix-8001/src/main/java/com/chennan/cloud/service/DeptService.java
+++ msc-provider-hystrix-8001/src/main/java/com/chennan/cloud/service/DeptService.java
@@ -11,8 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
