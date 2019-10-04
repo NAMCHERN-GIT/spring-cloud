480ff15d1ef518aa6072e4bdb794bcc63080d9ca
chen.nan <namchern@aliyun.com>
2019-10-04 20:55:09 +0800

    fastDfs 代码修改提交

--- msc-fdfs-provider-12000/src/main/java/com/chennan/cloud/MscFdfsProvider_12000_App.java
+++ msc-fdfs-provider-12000/src/main/java/com/chennan/cloud/MscFdfsProvider_12000_App.java
@@ -2,6 +2,9 @@ package com.chennan.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * spring cloud 整合FastDfs文件服务器实现上传下载功能
@@ -10,6 +13,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author chen.nan
  */
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableCircuitBreaker
 public class MscFdfsProvider_12000_App {
     public static void main(String[] args) {
         SpringApplication.run(MscFdfsProvider_12000_App.class, args);
